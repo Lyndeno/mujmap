bcacf43960679405b45dbec103acdaa401dd26cc
Lyndon Sanche <lsanche@lyndeno.ca>
2025-03-06 10:47:44 -0700

    flake: Fix build

--- flake.nix
+++ flake.nix
@@ -1,30 +1,116 @@
 {
   description = "Bridge for synchronizing email and tags between JMAP and notmuch";
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+
+    crane.url = "github:ipetkov/crane";
+
+    pre-commit-hooks-nix = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, cargo2nix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [cargo2nix.overlays.default];
+  outputs = { self, nixpkgs, utils, crane, pre-commit-hooks-nix}:
+  let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+  in
+    utils.lib.eachSystem systems (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      craneLib = crane.mkLib pkgs;
+      lib = pkgs.lib;
+
+      mdFilter = path: _type: builtins.match ".*md$" path != null;
+      mdOrCargo = path: type:
+        (mdFilter path type) || (craneLib.filterCargoSources path type);
+
+      src = lib.cleanSourceWith {
+        src = ./.;
+        filter = mdOrCargo;
+        name = "source";
+      };
+
+      common-args = {
+        inherit src;
+        strictDeps = true;
+
+        propagatedBuildInputs = [ pkgs.notmuch ];
+      };
+
+      cargoArtifacts = craneLib.buildDepsOnly common-args;
+
+      mujmap = craneLib.buildPackage (common-args
+        // {
+          inherit cargoArtifacts;
+        });
+
+      pre-commit-check = hooks:
+        pre-commit-hooks-nix.lib.${system}.run {
+          src = ./.;
+
+          inherit hooks;
         };
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.61.0";
-          packageFun = import ./Cargo.nix;
+    in rec {
+      checks = {
+        inherit mujmap;
+
+        mujmap-clippy = craneLib.cargoClippy (common-args
+          // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+        mujmap-fmt = craneLib.cargoFmt {
+          inherit src;
+        };
+
+        mujmap-deny = craneLib.cargoDeny {
+          inherit src;
+        };
+
+        pre-commit-check = pre-commit-check {
+          alejandra.enable = true;
+        };
+      };
+      packages.mujmap = mujmap;
+      packages.default = packages.mujmap;
+
+      apps.mujmap = utils.lib.mkApp {
+        drv = packages.mujmap;
+      };
+      apps.default = apps.mujmap;
+
+      formatter = pkgs.alejandra;
+
+      devShells.default = let
+        checks = pre-commit-check {
+          alejandra.enable = true;
+          #rustfmt.enable = true;
+          #clippy.enable = true;
         };
       in
-      {
-        packages = rec {
-          mujmap = ((rustPkgs.workspace.mujmap {}).overrideAttrs(oa: {
-            propagatedBuildInputs = oa.propagatedBuildInputs ++ [ pkgs.notmuch ];
-          })).bin;
-          
-          default = mujmap;
+        craneLib.devShell {
+          packages = with pkgs; [
+            rustfmt
+            clippy
+            cargo-deny
+            cargo-about
+            termshot
+            pkg-config
+            udev
+          ];
+          shellHook = ''
+            ${checks.shellHook}
+          '';
         };
-      });
+    })
+    // {
+      hydraJobs = {
+        inherit (self) checks packages devShells;
+      };
+    };
 }
